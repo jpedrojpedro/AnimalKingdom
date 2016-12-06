68eca84d9fdc87dde04f75849c1affe6ffb7acf1
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2016-12-06 19:55:53 -0200

    First Commit

---
+++ app/src/main/java/br/com/kidsapp/animalkingdom/FlappyBirdMainActivity.java
@@ -0,0 +1,13 @@
+package br.com.kidsapp.animalkingdom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlappyBirdMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flappy_bird);
+    }
+}
